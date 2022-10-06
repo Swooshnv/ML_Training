605851a12b3e4e8dc4cd7a129bf2569efa7d589e
Seyed Soroush Hosseini Naghavi <106426831+Swooshnv@users.noreply.github.com>
2022-10-07 01:08:52 +0330

    Update Asn_2_1.py

--- PyTorch/Asn_2_1.py
+++ PyTorch/Asn_2_1.py
@@ -36,9 +36,20 @@ X_train, y_train, X_test, y_test = train_test_split(X,
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class CircleModelV1(nn.Module):
-    def __init__():
+    def __init__(self):
         super().__init__()
         self.layer_1 = nn.Linear(in_features=2, out_features=5)
         self.layer_2 = nn.Linear(in_features=5, out_features=1)
     def forward(self, x):
-        #return self.
+        return self.layer_2(self.layer_1(x))
+
+model_0 = CircleModelV1().to(device)
+print(next(model_0.parameters()).device)
+
+### Replicate model using nn.sequential
+model_0 = nn.Sequential(nn.Linear(in_features=2, out_features=5),
+                        nn.Linear(in_features=5, out_features=1)).to(device)
+print(f"Replica: {next(model_0.parameters()).device}")
+
+### Setting up loss function and optimizer
+loss_fn = nn.BCEWithLogitsLoss()
