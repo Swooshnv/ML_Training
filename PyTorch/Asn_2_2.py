b10842ea6c910edf1af32715f6cd2a50a356505a
Seyed Soroush Hosseini Naghavi <106426831+Swooshnv@users.noreply.github.com>
2022-10-10 20:46:12 +0330

    Created a non-linear model with training/testing loop

--- PyTorch/Asn_2_2.py
+++ PyTorch/Asn_2_2.py
@@ -19,18 +19,58 @@ plt.scatter(x = X[: , 0],
             y = X[: , 1],
             c = y,
             cmap = plt.cm.RdYlBu)
-plt.show()            
+#plt.show()            
 
-X_train, y_train, X_test, y_test = train_test_split(X,
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y,
                                                     test_size = 0.2,
                                                     random_state = 42)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model_0 = nn.Sequential(
-    nn.Linear(in_features = 2, out_features = 10)
-    nn.Linear(in_features = 10, out_features = 10)
-    nn.Linear(in_features = 10, out_features = 1).to(device)
-)
+def accuracy_fn(y_true, y_preds):
+    correct = torch.eq(y_true, y_preds).sum().item()
+    acc = (correct / len(y_preds) * 100)
+    return acc
 
+class CircleModelV3(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features = 2, out_features = 10)
+        self.layer_2 = nn.Linear(in_features = 10, out_features = 10)
+        self.layer_3 = nn.Linear(in_features = 10, out_features = 1)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+
+model_2 = CircleModelV3().to(device)
+print(model_2)
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(params = model_2.parameters(),
+                            lr = 0.1)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+epochs = 1000
+
+for epoch in range(epochs):
+    model_2.train()
+    y_logits = model_2(X_train).squeeze()
+    y_preds = torch.round(torch.sigmoid(y_logits))
+    loss = loss_fn(y_logits, y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    acc = accuracy_fn(y_preds = y_preds, y_true = y_train)
+    model_2.eval()
+    with torch.inference_mode():
+        test_logits = model_2(X_test).squeeze()
+        test_preds = torch.round(torch.sigmoid(test_logits))
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true = y_test, y_preds = test_preds)
+    if epoch % 50 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f} | Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f} | Test Accuracy: {test_acc:.2f}%")
