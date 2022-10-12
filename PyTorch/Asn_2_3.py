9e33fcf4155012800e05e773dd72d2f3e84a3d2e
Seyed Soroush Hosseini Naghavi <106426831+Swooshnv@users.noreply.github.com>
2022-10-12 17:37:42 +0330

    Add files via upload

---
+++ PyTorch/Asn_2_3.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import sklearn 
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+
+NUM_CLASSES = 4
+NUM_FEATURES = 2
+RANDOM_SEED = 42
+
+X_blob, y_blob = make_blobs(n_samples = 1000,
+                            n_features = NUM_FEATURES,
+                            centers = NUM_CLASSES,
+                            cluster_std = 1.5,
+                            random_state = RANDOM_SEED)
+
+X_blob, y_blob = torch.from_numpy(X_blob).type(torch.float), torch.from_numpy(y_blob).type(torch.LongTensor)
+
+X_train, X_test, y_train, y_test = train_test_split(X_blob,
+                                                    y_blob,
+                                                    test_size = 0.2,
+                                                    random_state = RANDOM_SEED)
+
+plt.figure(figsize=(10, 7))
+plt.scatter(X_blob[:, 0], X_blob[:, 1], c = y_blob, cmap = plt.cm.RdYlBu)
+#plt.show()
+
+def accuracy_fn(y_true, y_preds):
+    correct = torch.eq(y_true, y_preds).sum().item()
+    acc = (correct / len(y_preds) * 100)
+    return acc
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+class blobClassificationV0(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units = 8):
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features = input_features, out_features = hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features = hidden_units, out_features = hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features = hidden_units, out_features = output_features)
+            )
+    def forward(self, x):
+        return self.linear_layer_stack(x)
+
+
+model_0 = blobClassificationV0(input_features = 2, output_features = 4).to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params = model_0.parameters(),
+                            lr = 0.1)
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+epochs = 1000
+for epoch in range(epochs):
+    model_0.train()
+    y_logits = model_0(X_train)
+    y_preds = torch.softmax(y_logits, dim = 1).argmax(dim = 1)
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_preds = y_preds, y_true = y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    model_0.eval()
+    with torch.inference_mode():
+        test_logits = model_0(X_test)
+        test_preds = torch.softmax(test_logits, dim = 1).argmax(dim = 1)
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_preds = test_preds, y_true = y_test)
+
+        if epoch % 50 == 0:
+            print(f"Epoch: {epoch} | Loss: {loss:.5f} | Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%")
