02eff9b8aa0cdfee099a6946ee4a2d9ff3a4e893
Seyed Soroush Hosseini Naghavi <106426831+Swooshnv@users.noreply.github.com>
2022-10-14 00:44:40 +0330

    added data loader for image datasets

--- PyTorch/Asn_3_1.py
+++ PyTorch/Asn_3_1.py
@@ -9,6 +9,7 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 print(torch.__version__)
 print(torchvision.__version__)
+BATCH_SIZE = 32
 
 ### Downloading Dataset
 train_data = datasets.FashionMNIST(
@@ -30,8 +31,30 @@ print(f"Length of training data: {len(train_data)}\nLength of testing data: {len
 class_names = train_data.classes
 class_to_idx = train_data.class_to_idx
 
+### Plotting one image
+"""
 image, label = train_data[0]
 plt_image = image.squeeze()
 plt.imshow(plt_image, cmap = "gray")
 plt.title(class_names[label])
 plt.show()
+"""
+### Plotting multiple images at once
+"""
+fig = plt.figure(figsize = (9, 9))
+rows, cols = 4, 4 
+for i in range(1, rows * cols + 1):
+    random_idx = torch.randint(0, len(train_data), size = [1]).item()
+    img, label = train_data[random_idx]
+    fig.add_subplot(rows, cols, i)
+    plt.imshow(img.squeeze(), cmap = "gray")
+    plt.title(class_names[label])
+    plt.axis(False)
+plt.show()
+"""
+train_dataloader = torch.utils.data.DataLoader(dataset = train_data, 
+                                               batch_size = BATCH_SIZE,
+                                               shuffle = True)
+test_dataloader = torch.utils.data.DataLoader(dataset = test_data,
+                                              batch_size = BATCH_SIZE,
+                                              shuffle = True)
